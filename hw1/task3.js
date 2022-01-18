d8754b43f413b046cc9f48fc91b474d1957dd08d
Olga Trojno <otronity@gmail.com>
2022-01-18 10:19:47 +0200

    revision

--- hw1/task3.js
+++ hw1/task3.js
@@ -1,6 +1,4 @@
-var val = 'b';
+var val = 'g';
 if (val == 'a') { console.log('a'); }
-else {
-    if (val == 'b' || val == 'c' || val == 'd' || val == 'e') { console.log('others'); }
+else if (val == 'b' || val == 'c' || val == 'd' || val == 'e') { console.log('others'); }
     else { console.log( 'unknown' );}
-}
